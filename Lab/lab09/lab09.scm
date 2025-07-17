c293958c1eea62970800fe1bd33c683e572f3322
Oiiiink <Xrt20060627@outlook.com>
2025-07-17 12:35:19 +0800

    Lab09 Finished at 2025/7/17 12:34, Congratulation!!!

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,16 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+        (cond ((< num1 num2) -1)
+             ((> num1 num2) 1)
+             (else 0)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+        (lambda (x) (+ x num)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+        (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+        (if (= n 1) f (composed f (repeat f (- n 1)))) )
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +22,11 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+        (if (= a (max a b))
+            (if (= 0 (modulo a b))
+                b
+                (gcd b (modulo a b)))
+            (if (= 0 (modulo b a))
+                a
+                (gcd a (modulo b a)))))
